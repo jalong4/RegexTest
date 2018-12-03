00118e204eeb7ffcd818d715eb14c95a15ffaf08
Jim Long <jalong4@hotmail.com>
2018-12-03 13:52:23 -0800

    Initial commit

---
+++ src/main/java/ca/jimlong/regextest/App.java
@@ -0,0 +1,11 @@
+package ca.jimlong.regextest;
+
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
